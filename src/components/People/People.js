5c70892c5ef8614e0f2ad397bff694afe16c1f54
Justyna Field <justyna.field@gmail.com>
2017-07-06 20:50:08 -0600

    render people data on the page

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Character from '../Character/Character';
 
 const People = ({characters}) => {
-console.log('characters:', characters)
-  const list = characters.map((person, i) => {
 
-    <Character key={i} {...person} />
-  });
+console.log('characters', characters)
+  let list = characters.map((obj, i) => {
+console.log('obj', obj)
+    return <Character key={i} {...obj} /> })
+console.log('list', list)
+
   return (
     <div>
+      <h2>characters</h2>
       {list}
     </div>
   )
